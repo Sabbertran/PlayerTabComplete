e342ea2898ebce88e74d4f10f52b0ce092eb8b47
Sabbertran <sabbertran@live.de>
2015-05-21 17:57:06 +0200

    v 1.1 - Removed external logging

--- src/de/sabbertran/playertabcomplete/PlayerTabComplete.java
+++ src/de/sabbertran/playertabcomplete/PlayerTabComplete.java
@@ -1,51 +1,18 @@
 package de.sabbertran.playertabcomplete;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class PlayerTabComplete extends Plugin
-{
-
-    public Logger log = getLogger();
+public class PlayerTabComplete extends Plugin {
 
     @Override
-    public void onDisable()
-    {
-//        log.info("PlayerTabComplete disabled");
+    public void onDisable() {
         System.out.println("PlayerTabComplete disabled");
     }
 
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         getProxy().getPluginManager().registerListener(this, new Events(this));
 
-        logStart();
-        
-//        log.info("PlayerTabComplete enabled");
         System.out.println("PlayerTabComplete enabled");
     }
-
-    private void logStart()
-    {
-
-        try
-        {
-            InetAddress adr = InetAddress.getLocalHost();
-            URL url = new URL("http://sabbertran.de/plugins/playertabcomplete/log.php?name=" + getProxy().getName() + "&ip=" + adr.getHostAddress() + "&port=bungee");
-            url.openStream();
-        } catch (UnknownHostException ex)
-        {
-            Logger.getLogger(PlayerTabComplete.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex)
-        {
-            Logger.getLogger(PlayerTabComplete.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
 }
