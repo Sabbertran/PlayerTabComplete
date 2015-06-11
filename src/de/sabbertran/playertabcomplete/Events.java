da682ebd6030c33a319fb5c761c5cc1ae96ac8ce
Sabbertran <sabbertran@live.de>
2015-06-11 09:57:00 +0200

    v 1.1.1 - Changed EventPriority to high to fix problems with some commands

--- src/de/sabbertran/playertabcomplete/Events.java
+++ src/de/sabbertran/playertabcomplete/Events.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.TabCompleteEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 public class Events implements Listener
 {
@@ -15,7 +16,7 @@ public class Events implements Listener
         this.main = main;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onTabComplete(TabCompleteEvent ev)
     {
         String partialPlayerName = ev.getCursor().toLowerCase();
